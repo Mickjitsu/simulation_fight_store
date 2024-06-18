a0d28e9d80ea97c81a01a8dc2f6a9e814e0d66d5
Michael McCourt <mmccourt928@outlook.com>
2024-06-18 16:32:29 +0000

    main.py removed in order to update later

--- fightstore/main.py
+++ fightstore/main.py
@@ -1,7 +1,1 @@
-import sheets
-from inventory import get_inventory
 
-def welcome():
-    print("Welcome to Micks Simulation fight store")
-    print("Please choose a category to view our products")
-    print("Kimonos \n Rashguards \n Shorts \n Protective Wear")
